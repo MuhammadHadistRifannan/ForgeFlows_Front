8970821a5835c97fef8722094035be8f7d14cdfd
MuhammadHadistRifannan <ipanjabar933@gmail.com>
2026-07-23 03:53:39 +0700

    build

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Hello World
+      <Image
+      src="/assets/images.jpeg"
+      alt="Logo"
+      width={150}
+      height={50}
+    />
     </div>
   );
 }
